d7ae17a29e35d23e18ef077ee7b5114ff1bd00bb
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2022-04-26 17:25:56 +0100

    TP GraphQl : ex 39,57,71

--- data/db.js
+++ data/db.js
@@ -40,5 +40,70 @@ export const db = {
             id: 3,
             designation: 'GL4'
         },
+    ],
+    todos: [
+        {
+            id:1,
+            name:"TP GraphQl",
+            content:"do tasks before wednesday",
+            status:"wait",
+            user:2
+
+        },
+        {
+            id:2,
+            name:"Champions league",
+            content:"match starts at 20:00",
+            status:"in_progress",
+            user:1
+
+        },
+        {
+            id:3,
+            name:"revision cours nest",
+            content:"le cours de lundi diapo 230",
+            status:"done",
+            user:3
+
+        },
+        {
+            id:4,
+            name:"fishing",
+            content:"sunday morning on the beach",
+            status:"canceled",
+            user:2
+
+        },
+        {
+            id:5,
+            name:"working on PPP",
+            content:"sprint retrospective 17hr",
+            status:"postponed",
+            user:4
+
+        },
+    ],
+    users:[
+        {
+            id:1,
+            name:"racem",
+            email:"racem@outlook.com"
+        },
+        {
+            id: 2,
+            name:"benrhayem",
+            email:"benrhayem@yahoo.com"
+        },
+        {
+            id:3,
+            name:"graghQl",
+            email:"graghQl@gmail.com"
+        },
+        {
+            id:4,
+            name:"nestJs",
+            email:"nestJs@gmail.com"
+        },
+
     ]
 }
