24659e669f14046108058b7f83e1eac4dc33d6ea
Racem Benrhayem <racembenrhayem@insat.u-cartage.tn>
2022-04-27 14:40:28 +0100

    fix/userFilterTodo

--- resolvers/User.js
+++ resolvers/User.js
@@ -1,7 +1,7 @@
 
 export const User = {
     todos: ( user,args,{db} ) => {
-        return db.todos.find(
+        return db.todos.filter(
             (todo) => todo.user == user.id
         );
     }
